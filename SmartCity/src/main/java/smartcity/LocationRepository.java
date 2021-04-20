e9c59e2af1c2f9a60c79230765c7eae0868c6795
Arshia Hsn <arshiahsn@gmail.com>
2021-04-20 13:08:13 -0600

    Delete SmartCity/src/main/java directory

--- SmartCity/src/main/java/smartcity/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package smartcity;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface LocationRepository extends CrudRepository<Location, Long> {
-	List<Location> findByLatitudeAndLongitude(double latitude, double longitude);
-}
